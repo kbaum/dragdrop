e388d7d26839c45d1d492eb7747375796b015739
Karl <karl.baum@gmail.com>
2011-09-15 08:17:51 -0400

    installed cucumber-rails and capybara with webkit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dragdrop::Application.config.secret_token = '9521d8a7ce3d4243c9092362bfe5ddd02c7b3b56db686e34c285c93dd3c8fb3fbc7a1e65bb46709b65d5b1d551efdce36557a9e668914c44393c2084d0b3d348'
